c4d8fe9ec3ec5aa5efc6594915ff527e7ff346e8
Asadbek <asadbekoffical1202@gmail.com>
2025-12-18 17:17:33 +0500

    Update data

--- loader.py
+++ loader.py
@@ -1,7 +1,7 @@
 from aiogram import Bot
 from aiogram.enums import ParseMode
 from aiogram.client.default import DefaultBotProperties
-from .data import config
+from data import config
 from utils.db_api.sqlite import Database
 
 bot = Bot(
